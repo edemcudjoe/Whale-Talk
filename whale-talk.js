a2708c31c3136fccf6941dc9ab10729716343f2a
Edem Cudjoe <edem.cudjoe@amalitech.org>
2022-01-24 19:45:30 +0000

    initial commit

---
+++ whale-talk.js
@@ -0,0 +1,18 @@
+let input = 'a whale of a deal';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  //console.log(i);
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    }
+    //console.log(j);
+  }
+  if (input[i] === 'e' || input[i] === 'u') {
+    resultArray.push(input[i]);
+  }
+}
+
+console.log(resultArray.join('').toUpperCase());
